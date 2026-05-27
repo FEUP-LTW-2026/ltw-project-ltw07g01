f72173ad85bc2bc2850d482fd89fabc7dfec2783
David <2006davidferreira@gmail.com>
2026-05-27 01:52:36 +0100

    removed

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
     require_once(__DIR__ . '/utils/session.php');
-
     $session = new Session();
-    $destination = $session->isLoggedIn() ? '/pages/dashboard.php' : '/pages/index.php';
 
-    header('Location: ' . $destination);
+    header('Location:/pages/index.php');
     exit();
 ?>
